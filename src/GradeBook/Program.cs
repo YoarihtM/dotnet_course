ade7e0139cbea30dc159c58c7579b3af7d8db8c3
Yoariht Macedo <yoariht.mc37@gmail.com>
2023-10-03 18:38:10 -0600

    dotnet fundamentals

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GradeBook{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
